29682290c4cf4c5df8508b39407b635b58216391
Ameskosu <kobayashikosuke248@gmail.com>
2024-05-02 11:53:26 +0900

    fix_showhistorybutton updated

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -111,7 +111,20 @@ const Demo = () => {
           </button>
         </form>
 
-        <button onClick={() => handleshow()}>show browse history</button>
+        <button onClick={() => handleshow()} className="text-gray-200">
+          {historyshowflag?
+            <div className="flex justify-center">
+              <svg class="w-6 h-2 text-gray-200 hover:text-gray-300 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"/>
+              </svg>  
+            </div>
+          : <div className="flex justify-center">
+              <svg class="w-6 h-2 text-gray-200 hover:text-gray-300 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
+              </svg>
+            </div>
+          }
+        </button>
 
         {/* Browse History */}
         {historyshowflag ?
@@ -160,7 +173,9 @@ const Demo = () => {
                 <p className='font-inter font-medium text-sm text-gray-200'>
                   {article.summary}
                 </p>
-                <button onClick={handleDownloadSummary}>Download Summary</button>
+                <div className="flex justify-end mr-6">
+                <button onClick={handleDownloadSummary} className="text-green-400 hover:text-green-300">Download</button>
+                </div>
               </div>
             </div>
           )
